d3886b3fc527c322b3880908e430c831a1adc0ea
masatonara <masato.nara83@gmail.com>
2021-04-18 10:03:14 +0000

    トップページ　aboutページ　ヘッダー　コントローラー作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,14 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :kana_last_name, presence: true
+  validates :postal_code, presence: true, numericality: { only_integer: true}, length: { is: 7 }
+  validates :address, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true}
 end
