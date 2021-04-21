7fabbb69501376432a58d6c5b6c36234ee9384d5
masatonara <masato.nara83@gmail.com>
2021-04-21 08:51:22 +0000

    admin/product完成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,12 @@
 class Product < ApplicationRecord
-
   belongs_to :genre
-
-  has_many :order_datails, dependent: :destroy
-
-  has_many :cart_items, dependent: :destroy
+  has_many :cart_items
+  has_many :orders, through: :order_details
+  has_many :order_details
 
   attachment :image
-	has_many :orders, through: :order_details
-	has_many :order_details
-	validates :genre_id, :name, :tax_out_price, presence: true
-	validates :explanation, length: {maximum: 200}
-	validates :tax_out_price, numericality: { only_integer: true }
+  
+	validates :genre_id, :name, :price, presence: true
+	validates :introduction, length: {maximum: 200}
+	validates :price, numericality: { only_integer: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 2021_04_20_055304) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
-    t.string "name", null: false
-    t.string "address", null: false
-    t.string "postal_code", null: false
+    t.string "name"
+    t.string "address"
+    t.string "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -62,7 +62,8 @@ ActiveRecord::Schema.define(version: 2021_04_20_055304) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
+    t.boolean "is_valid", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -99,18 +100,6 @@ ActiveRecord::Schema.define(version: 2021_04_20_055304) do
     t.boolean "is_active", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
