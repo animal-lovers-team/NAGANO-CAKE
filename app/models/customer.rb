9eab7d927f9f63cac5dbb12a3d30372ae5a693a7
EC2 Default User <mkhrauau@gmail.com>
2021-04-21 08:55:16 +0000

    admin_customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,14 +1,32 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+ devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :addresses, dependent: :destroy
-  has_many :orders
+
+  has_many :orders, dependent: :destroy
+
+  #フルネームメソッド
+  def full_name
+    self.last_name + self.first_name
+  end
+
+  #フルネームカナメソッド
+  def kana_full_name
+    self.kana_last_name + self.kana_first_name
+  end
+
+
+  has_many :deliver_destinations, dependent: :destroy
   has_many :cart_items, dependent: :destroy
 
-  
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :kana_last_name, presence: true
+  validates :postal_code, presence: true, numericality: { only_integer: true }, length: { is: 7 }
+  validates :street_address, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }
 
 end
