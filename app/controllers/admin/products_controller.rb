b0f00e11b25d8a4d007014d04268434435a8c41f
EC2 Default User <mkhrauau@gmail.com>
2021-04-24 08:36:02 +0000

    admin_product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -23,14 +23,15 @@ class Admin::ProductsController < ApplicationController
   end
 
   def index
-
     @products = Product.all.page(params[:page]).per(10)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
