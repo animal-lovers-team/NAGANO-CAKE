8ddef928adfcdf54f4ce908555f4afa9ea9672c4
masatonara <masato.nara83@gmail.com>
2021-04-24 07:27:08 +0000

    order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,4 @@
 module ApplicationHelper
-<<<<<<< HEAD
-=======
 
   def current_cart
     @cart_items = current_customer.cart_items
@@ -8,31 +6,10 @@ module ApplicationHelper
 
   # 小数点切り捨てはfloor,３桁区切りはto_s(:delimited)
   # 税込の計算
->>>>>>> 2c965ac29046cff43021baf87e41ced314db798c
   def tax_price(price)
     (price * 1.1).floor
   end
 
-<<<<<<< HEAD
-  def sub_price(sub)
-    (tax_price(sub..price) * sub.quantity)
-  end
-
-  def total_price(totals)
-    price = 0
-    totals.each do |total|
-      price += sub_price(total)
-    end
-    return price
-  end
-  
-  def current_cart
-    @cart_items = current_customer.cart_items
-  end
-  
-  def billing(order)
-    total_price(current_cart) + order.postage 
-=======
   # 小計の計算
   def sub_price(sub)
     (tax_price(sub.product.price) * sub.quantity)
@@ -50,7 +27,5 @@ module ApplicationHelper
   # 請求額の計算
   def billing(order)
     total_price(current_cart) + order.postage
->>>>>>> 2c965ac29046cff43021baf87e41ced314db798c
   end
 end
-
