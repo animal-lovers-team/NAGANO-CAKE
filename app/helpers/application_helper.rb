683a4c197243f1c821e42edbcd00fcd3bc2db018
koki-takahashi-1107 <jubeatknit18@gmail.com>
2021-04-25 02:45:56 +0000

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,5 @@
 module ApplicationHelper
 
-   def total_price(totals)
-    price = 0
-    totals.each do |total|
-      price  +=  sub_price(total)
-     end
-     return price
-   end
-
-
-   def billing(order)
-    total_price(current_customer) + order.postage
-   end
-
   def current_cart
     @cart_items = current_customer.cart_items
   end
